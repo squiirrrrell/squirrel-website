49c9b120d8b8cabe6a21bf4b325fb5f7c9f653a3
Squirrel <squiirrrrell@gmail.com>
2025-03-29 05:57:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
-import path from "path";
 
-export default {
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
-};
+});
